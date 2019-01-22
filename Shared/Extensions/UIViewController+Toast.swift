e0b98bef43dae1f48fc7454db72024910934ea38
Michael Hill <michaelhill@Michaels-MBP.home>
2019-01-22 12:49:44 -0500

    HUGE cleanup of UX / layout. centered watch, moved to extension based toast for messages, cleanup all over storyboards

---
+++ Shared/Extensions/UIViewController+Toast.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Toast.swift
+//  AppleWatchFaces
+//
+//  Created by Michael Hill on 1/22/19.
+//  Copyright © 2019 Michael Hill. All rights reserved.
+//
+
+import Foundation
